b0e96437b947fd297e70ccd3ddf80b07951c1149
canoh5206-oss <canoh5206@gmail.com>
2026-05-29 19:50:00 +0300

    Initialize Discord bot with ping command

---
+++ index.js
@@ -0,0 +1,21 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.on("ready", () => {
+  console.log("Bot açıldı!");
+});
+
+client.on("messageCreate", (message) => {
+  if (message.content === "!ping") {
+    message.reply("Pong 🏓");
+  }
+});
+
+client.login(process.env.TOKEN);
