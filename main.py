ca7a82859163ff423ea18c3c40f19b15530454f5
canoh5206-oss <canoh5206@gmail.com>
2026-05-29 15:26:14 +0300

    Add Discord bot setup with intents and on_ready event

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-import os
+import discord
+from discord.ext import commands
 
-# Botu çalıştıran tam kod satırı:
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Bot aktif: {bot.user}')
+
 bot.run("MTUwOTg0ODMyMDU1NjkyNDk0OA.GPGdzq.NyYH1iOF9mUvpaAvEqTbvpWva5qjJIrZXVWMBU")
