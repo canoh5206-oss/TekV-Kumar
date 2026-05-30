372e6358fee7b39cbe8ddb9b74d455320e59e7ae
canoh5206-oss <canoh5206@gmail.com>
2026-05-30 19:58:44 +0300

    Remove discord bot implementation from main.py

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-import discord
-from discord.ext import commands
 
-intents = discord.Intents.default()
-intents.message_content = True
-
-bot = commands.Bot(command_prefix="!", intents=intents)
-
-@bot.event
-async def on_ready():
-    print(f'Bot aktif: {bot.user}')
-
-bot.run(MTUwOTg0ODMyMDU1NjkyNDk0OA.GPGdzq.NyYH1iOF9mUvpaAvEqTbvpWva5qjJIrZXVWMBU)
